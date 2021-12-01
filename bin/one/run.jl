544ce256d6db6ff19a3a07ca9fe5bea9a16e7c6a
alisdair <alisdairsullivan@gmail.com>
2021-12-01 08:17:12 -0800

    aoc2021 :: day one cleanup

--- bin/one/run.jl
+++ bin/one/run.jl
@@ -1,32 +1,15 @@
 using DelimitedFiles
+using RollingFunctions
 
 input = joinpath(@__DIR__, "input")
 entries = readdlm(input, Int)
 
 function depth(entries)
-    acc = 0
-    for i in 1:(length(entries) - 1)
-        if entries[i] < entries[i + 1]
-            acc += 1
-        end
-    end
-
-    return acc
+    return length(filter!(n -> n > 0, diff(entries)))
 end
 
-function windowed_depth(entries)
-    acc = 0
-    for i in 1:(length(entries) - 3)
-        if sum(entries[i:i + 2]) < sum(entries[i + 1:i + 3])
-            acc += 1
-        end
-    end
-
-    return acc
-end
-
-p1 = depth(entries)
-p2 = windowed_depth(entries)
+p1 = depth(vec(entries))
+p2 = depth(rolling(sum, vec(entries), 3))
 
 @assert(p1 == 1154)
 @assert(p2 == 1127)
