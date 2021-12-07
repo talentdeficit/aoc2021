bd8299010d89a2070497dd033ee5e796aff41b02
alisdair <alisdairsullivan@gmail.com>
2021-12-06 21:32:32 -0800

    aoc2021 :: day seven

--- bin/seven/run.jl
+++ bin/seven/run.jl
@@ -5,14 +5,14 @@ crabs = readdlm(input, ',', Int)
 
 function align_crabs(crabs, fuel_calc)
     acc = []
-    for i in 1:maximum(crabs)
+    for i in minimum(crabs):maximum(crabs)
         append!(acc, sum(map(n -> fuel_calc(i, n), crabs)))
     end
     return acc
 end
 
 p1 = minimum(align_crabs(crabs, (i, n) -> abs(i - n)))
-p2 = minimum(align_crabs(crabs, (i, n) -> sum(1:abs(i - n))))
+p2 = minimum(align_crabs(crabs, (i, n) -> div(abs(i - n) * (abs(i - n) + 1), 2)))
 
 @assert(p1 == 343468)
 @assert(p2 == 96086265)
