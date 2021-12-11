ff7d0f41f3218c4cfb38995508e0f1032f61b05d
alisdair <alisdairsullivan@gmail.com>
2021-12-10 16:10:16 -0800

    aoc2021 :: cleanup

--- bin/one/run.jl
+++ bin/one/run.jl
@@ -5,16 +5,16 @@ input = joinpath(@__DIR__, "input")
 entries = readdlm(input, Int)
 
 function depth(entries)
-    return length(filter!(n -> n > 0, diff(entries)))
+    return length(filter(n -> n > 0, diff(entries)))
 end
 
 p1 = depth(vec(entries))
 p2 = depth(rolling(sum, vec(entries), 3))
 
-@assert(p1 == 1154)
-@assert(p2 == 1127)
-
 println("-----------------------------------------------------------------------")
 println("sonar sweep -- part one :: $p1")
 println("sonar sweep -- part two :: $p2")
 println("-----------------------------------------------------------------------")
+
+@assert(p1 == 1154)
+@assert(p2 == 1127)
